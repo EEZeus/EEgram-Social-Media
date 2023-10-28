79fd813494e7fb7c2152d2f2bac0cb11ec7fba3e
= <=>
2023-10-28 22:02:48 +0330

    login & signup pages are created

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,33 @@
-import './Login.scss'
+import { Link } from "react-router-dom";
+import "./Login.scss";
 
-function Login(){
-    return(
-        <div className = 'login'>Login</div>
-    )
+function Login() {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>EEgram</h1>
+          <p>
+            Unlock a world of tailored connections and interactive experiences
+            with EEgram. As you embark on your journey through this innovative
+            social platform, expect more than just a typical networking site.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to={'/signup'}>
+          <button>Sign up</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Log in</h1>
+          <form action="">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Log in</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom';
 import './Signup.scss'
 
-function Signup(){
-    return(
-        <div className = 'signup'>Signup</div>
-    )
-}
+function Signup() {
+    return (
+      <div className="signup">
+        <div className="card">
+          <div className="left">
+            <h1>EEgram</h1>
+            <p>
+              Unlock a world of tailored connections and interactive experiences
+              with EEgram. As you embark on your journey through this innovative
+              social platform, expect more than just a typical networking site.
+            </p>
+            <span>Do you have an account?</span>
+            <Link to={'/login'}>
+            <button>Log in</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1>Sign up</h1>
+            <form action="">
+              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <button>Sign up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default Signup;
