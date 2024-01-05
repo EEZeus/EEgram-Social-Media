80f1fe2c1422ec788365e6f56a9d0a88a3daf9b9
= <=>
2024-01-05 18:54:47 +0330

    fetching  posts  is done. upload has bug.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import storiesRoute from "./routes/stories.js";
 import authRoute from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import multer from "multer";
 const app = Express();
 //middleware
 app.use((req,res,next)=>{
@@ -20,7 +21,21 @@ app.use(cors({
 ))
 app.use(cookieParser())
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/eegram/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null,Date.now()+file.originalname)
+  }
+})
 
+const upload = multer({ storage: storage })
+
+app.post('/server/upload',upload.single('file'), (req,res)=>{
+ const file = req.file;
+  res.status(200).json(file.filename)
+})
 app.use("/server/users", usersRoute);
 app.use("/server/posts", postsRoute);
 app.use("/server/likes", likesRoute);
