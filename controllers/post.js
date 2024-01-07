e8e2d91664edbc8e3fd0d2a41477da0e112a00de
= <=>
2024-01-07 22:29:11 +0330

    update&fetch user, delete post, implemented.

--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,29 @@
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
-import moment from 'moment'
+import moment from "moment";
 export const getPosts = (req, res) => {
+  const userId = req.query.userId;
   const token = req.cookies.accessToken;
+
   if (!token) return res.status(401).json("Not Logged In !");
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).josn("Token is not valid !");
 
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedId) WHERE r.followerId = ? OR p.userId = ? ORDER BY p.createdAt DESC`;
+    let q =``;
+    if (userId ==='undefined') {
+      q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedId) WHERE r.followerId = ? OR p.userId = ? ORDER BY p.createdAt DESC`
+    } else {
+      q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ?`;
+    }
+    let values = [];
+    if (userId ==='undefined') {
+      values = [userInfo.id, userInfo.id];
+    } else {
+      values = [userId];
+    }
 
-    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+    db.query(q, values, (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
     });
@@ -18,23 +31,42 @@ export const getPosts = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not Logged In !");
-  
-    jwt.verify(token, "secretkey", (err, userInfo) => {
-      if (err) return res.status(403).josn("Token is not valid !");
-  
-      const q = "INSERT INTO posts (`desc`,`img`,`createdAt`,`userId`) VALUES (?)";
-  
-      const values = [
-        req.body.desc,
-        req.body.img,
-        moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-        userInfo.id
-      ]
-      db.query(q, [values], (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json(data);
-      });
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged In !");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid !");
+    const q =
+      "INSERT INTO posts (`desc`,`img`,`createdAt`,`userId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
     });
-  };
+  });
+};
+
+export const deletePost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged In !");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid !");
+
+    const q =
+      "DELETE FROM posts WHERE `id`=? AND `userId`=?";
+
+    
+    db.query(q, [req.params.id,userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if(data.affectedRows>0) return res.status(200).json('Post has been deleted.')
+      return res.status(403).json('You can delete only your post!');
+    });
+  });
+};
